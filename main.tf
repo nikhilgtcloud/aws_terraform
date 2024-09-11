0ffda549d554e68d7db4f1b87884d5e5f6711bba
Nikhil Thakur <nikhil.gtcloud@gmail.com>
2024-09-11 16:31:52 +0530

    modified main.tf file

--- main.tf
+++ main.tf
@@ -2,6 +2,18 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "my-unique-bucket-name111111111222"
+resource "aws_instance" "mumbai" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"  # Free tier eligible
+}
+
+provider "aws" {
+  region = "ap-south-2"
+  alias = "hyd"
+}
+
+resource "aws_instance" "hyderabad" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"  # Free tier eligible
+  provider = aws.hyd
 }
