472e3923878d5aadec71cd9bb5ec0f45bb660298
Nikhil Thakur <nikhil.gtcloud@gmail.com>
2024-09-11 16:52:55 +0530

    modified main.tf file

--- main.tf
+++ main.tf
@@ -2,18 +2,8 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_instance" "mumbai" {
-  ami           = "ami-0e53db6fd757e38c7"
-  instance_type = "t2.micro"  # Free tier eligible
-}
+#resource "aws_instance" "mumbai" {
+#  ami           = "ami-0e53db6fd757e38c7"
+#  instance_type = "t2.micro"  # Free tier eligible
+#}
 
-provider "aws" {
-  region = "ap-southeast-1"
-  alias = "hyd"
-}
-
-resource "aws_instance" "hyderabad" {
-  ami           = "ami-04a5ce820a419d6da"
-  instance_type = "t2.micro"  # Free tier eligible
-  provider = aws.hyd
-}
